54e0031a69d66d07884b904c97c3a27cb10b1f9e
ringodev <ringodev@protonmail.com>
2020-11-08 17:52:34 +0100

    chenged backend url

--- src/app/environment/evironment.ts
+++ src/app/environment/evironment.ts
@@ -3,7 +3,7 @@ let url:string
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     url = 'http://localhost:8080/api'
 } else {
-    url = 'https://www.ringodev.xyz:8443/api'
+    url = 'https://backend.ringodev.com:8443/api'
 }
 
 export const BACKEND_URL = url
